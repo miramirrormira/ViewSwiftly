8f61c2cab5aae05d879023698f7d65c9302bc99b
Mira Yang <miracolly330@gmail.com>
2024-05-08 13:49:14 -0400

    modify gitignore file

--- Package.swift
+++ Package.swift
@@ -5,17 +5,23 @@ import PackageDescription
 
 let package = Package(
     name: "ViewSwiftly",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ViewSwiftly",
             targets: ["ViewSwiftly"]),
     ],
+    dependencies: [.package(url: "https://github.com/miramirrormira/NetSwiftly.git", branch: "main")],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ViewSwiftly"),
+            name: "ViewSwiftly",
+            dependencies: ["NetSwiftly"]),
         .testTarget(
             name: "ViewSwiftlyTests",
             dependencies: ["ViewSwiftly"]),
