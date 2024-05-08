c56be14fff2445ec438566656a4048bb1b9e6bec
Mira Yang <miracolly330@gmail.com>
2024-05-08 13:44:30 -0400

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ViewSwiftly",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "ViewSwiftly",
+            targets: ["ViewSwiftly"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "ViewSwiftly"),
+        .testTarget(
+            name: "ViewSwiftlyTests",
+            dependencies: ["ViewSwiftly"]),
+    ]
+)
